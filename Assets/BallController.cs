38d6b6aed72137a1a60df71ccdf11b3092778c0a
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2025-01-30 11:48:50 -0800

    Added 10 cylinders to act as pins

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,18 +6,19 @@ public class BallController : MonoBehaviour
     [SerializeField] private float force = 1f;
     [SerializeField] private InputManager inputManager;
 
+    private bool isBallLaunched;
     private Rigidbody ballRB;
 
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
-
-        //Listener
         inputManager.OnSpacePressed.AddListener(LaunchBall);
     }
 
     private void LaunchBall()
     {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
