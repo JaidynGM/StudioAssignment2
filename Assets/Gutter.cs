fffdc47aca81490d0bef1c98a69ee6c4367bdb2a
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2025-02-03 14:38:49 -0800

    added other cylinder and cleaned up code

--- Assets/Gutter.cs
+++ Assets/Gutter.cs
@@ -1,20 +1,16 @@
 using UnityEngine;
 
- public class Gutter : MonoBehaviour
- {
- private void OnTriggerEnter(Collider triggeredBody)
- {
- 
- Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+public class Gutter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider triggeredBody)
+    {
+        Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
 
- Debug.Log($"Ball {triggeredBody.name} entered the gutter.");
+        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
 
- float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+        ballRigidBody.linearVelocity = Vector3.zero;
+        ballRigidBody.angularVelocity = Vector3.zero;
 
- ballRigidBody.linearVelocity = Vector3.zero;
- ballRigidBody.angularVelocity = Vector3.zero;
-
-    
- ballRigidBody.AddForce(transform.up * velocityMagnitude, ForceMode.VelocityChange);
- }
- }
+        ballRigidBody.AddForce(transform.up * velocityMagnitude, ForceMode.VelocityChange);
+    }
+}
