22497d300c420262dbffcb74fd2076b2aa2ed375
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2025-02-03 14:35:55 -0800

    Fixed gutter issue

--- Assets/Gutter.cs
+++ Assets/Gutter.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 
-public class Gutter : MonoBehaviour
-{
-   private void OnTriggerEnter(Collider triggeredBody)
-    {
-        Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+ public class Gutter : MonoBehaviour
+ {
+ private void OnTriggerEnter(Collider triggeredBody)
+ {
+ 
+ Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
 
-        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+ Debug.Log($"Ball {triggeredBody.name} entered the gutter.");
 
-        ballRigidBody.linearVelocity = Vector3.zero;
-        ballRigidBody.angularVelocity = Vector3.zero; 
+ float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
 
-        ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
-    }
-}
+ ballRigidBody.linearVelocity = Vector3.zero;
+ ballRigidBody.angularVelocity = Vector3.zero;
+
+    
+ ballRigidBody.AddForce(transform.up * velocityMagnitude, ForceMode.VelocityChange);
+ }
+ }
