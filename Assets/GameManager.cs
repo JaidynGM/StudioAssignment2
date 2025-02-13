8961018c2b20b43377ec66fb3d718b149d791259
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2025-02-12 17:39:55 -0800

    Added scoring system

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,11 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
     private FallTrigger[] pins;
 
     private void Start()
@@ -19,5 +21,6 @@ public class GameManager : MonoBehaviour
     private void IncrementScore()
     {
         score++;
+        scoreText.text = $"Score: {score}";
     }
 }
